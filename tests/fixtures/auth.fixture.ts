15938c6029a4de441770fefd3b767c8057ac34af
David Lake <david.lake@citizensadvice.org.uk>
2026-03-03 10:35:03 +0000

    Move app class

--- tests/fixtures/auth.fixture.ts
+++ tests/fixtures/auth.fixture.ts
@@ -1,6 +1,6 @@
 import { test as base, expect } from '@playwright/test';
 import { createUser, deleteUserByEmail } from '@factories/user.factory';
-import { App } from '../app';
+import { App } from '@pages/app';
 
 export interface TestUser {
   username: string;

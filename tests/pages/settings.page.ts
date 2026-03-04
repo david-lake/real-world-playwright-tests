0f321e4860a62892cdf635878b1623b88f7dc40f
David Lake <david.lake@citizensadvice.org.uk>
2026-03-04 15:36:28 +0000

    Remove unused code

--- tests/pages/settings.page.ts
+++ tests/pages/settings.page.ts
@@ -11,18 +11,6 @@ export class SettingsPage {
     await expect(this.page.getByRole('heading', { name: 'Your settings' })).toBeVisible();
   }
 
-  async updateBio(bio: string) {
-    await this.page.getByPlaceholder('Short bio about you').fill(bio);
-  }
-
-  async submit() {
-    await this.page.getByRole('button', { name: 'Update Settings' }).click();
-  }
-
-  async expectBio(bio: string) {
-    await expect(this.page.getByPlaceholder('Short bio about you')).toHaveValue(bio);
-  }
-
   async logout() {
     await this.page.getByRole('button', { name: 'Click here to logout' }).click();
   }

aebcfabdd1210c8ceeea629b54f30816db4d645a
David Lake <david.lake@citizensadvice.org.uk>
2026-03-12 12:02:00 +0000

    Extend test coverage

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -7,20 +7,31 @@ import { createUser } from '@factories/user.factory';
 test.describe('Comments', () => {
 
   test.describe('Guests', () => {
-    test('Prevent commenting when not logged in', async ({ app }) => {
-      const author = await createUser();
-      const article = await createArticle(author.id);
+    test('Prevent commenting on articles when not logged in', async ({ app, testUser }) => {
+      const article = await createArticle(testUser.id);
 
       await app.article.open(article.slug);
       await app.article.expectLoaded();
       await app.header.expectLoggedOut();
-
+      
       await expect(app.article.commentTextarea()).toHaveCount(0);
 
       const guestPrompt = app.page.getByText('Sign in or sign up to add comments on this article.');
       await expect(guestPrompt).toBeVisible();
       await expect(guestPrompt.getByRole('link', { name: 'Sign in' })).toBeVisible();
       await expect(guestPrompt.getByRole('link', { name: 'Sign up' })).toBeVisible();
+    });
+
+    test('Allowed to see comments on articles when not logged in', async ({ app, testUser }) => {
+      const article = await createArticle(testUser.id);
+      const commentText = `Comment ${Date.now()}`;
+      await createComment(article.id, testUser.id, commentText);
+
+      await app.article.open(article.slug);
+      await app.article.expectLoaded();
+      await app.header.expectLoggedOut();
+
+      await app.article.expectCommentVisible(commentText, testUser.username);
     });
   });
 
@@ -30,6 +41,16 @@ test.describe('Comments', () => {
       await app.login.login(testUser.email, testUser.plainPassword);
       await app.home.expectLoaded();
       await app.header.expectLoggedIn(testUser.username);
+    });
+
+    test('Add comment to own article', async ({ app, testUser }) => {
+      const article = await createArticle(testUser.id)
+      const commentText = `Test comment ${Date.now()}`;
+
+      await app.article.open(article.slug)
+      await app.article.postComment(commentText);
+
+      await app.article.expectCommentVisible(commentText, testUser.username);
     });
 
     test("Add comment to another author's article", async ({ app, testUser }) => {

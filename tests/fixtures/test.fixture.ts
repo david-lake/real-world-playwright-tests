f76fa865cd62a2ead27c2e77949ffeaa2d4a7e13
David Lake <david.lake@citizensadvice.org.uk>
2026-03-12 23:05:46 +0000

    All fixtures in one test fixture

--- tests/fixtures/test.fixture.ts
+++ tests/fixtures/test.fixture.ts
@@ -1,10 +1,21 @@
-import { test as base, expect } from '@fixtures/app.fixture';
+import { test as base, expect } from '@playwright/test';
+import { App } from 'tests/app/app';
+import { resetDatabase } from '../db/cleaner';
 import { createUser, type TestUser } from '@factories/user.factory';
 
 export const test = base.extend<{
+  dbCleaner: void;
+  app: App;
   testUser: TestUser;
 }>({
-  testUser: async ({ dbCleaner }, use) => {
+  dbCleaner: async ({}, use) => {
+    await resetDatabase();
+    await use();
+  },
+  app: async ({ page }, use) => {
+    await use(new App(page));
+  },
+  testUser: async ({}, use) => {
     const user = await createUser();
 
     await use(user);
